59129e40bfa25b1588bcc11829f71ef62c174afd
wenquansun <604164227@qq.com>
2022-09-26 18:35:51 +0800

    add mergeTwoLists

--- 21.合并两个有序链表.js
+++ 21.合并两个有序链表.js
@@ -58,16 +58,87 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
-/**
+// /** 1 特点: 不改变原链表
+//  * @param {ListNode} list1
+//  * @param {ListNode} list2
+//  * @return {ListNode}
+//  */
+// var mergeTwoLists = function (list1, list2) {
+//   const arr = []
+//   while (list1 || list2) {
+//     const node = new ListNode()
+//     let value = 0
+//     if (!list1) {
+//       value = list2.val
+//       list2 = list2.next
+//     } else if (!list2) {
+//       value = list1.val
+//       list1 = list1.next
+//     } else if (list1.val < list2.val) {
+//       value = list1.val
+//       list1 = list1.next
+//     } else {
+//       value = list2.val
+//       list2 = list2.next
+//     }
+//     node.val = value
+//     arr.push(node)
+//   }
+//   if (arr.length < 1) {
+//     return null
+//   }
+//   let first = arr[0]
+//   for (let i = 1; i < arr.length; i++) {
+//     first.next = arr[i]
+//     first = first.next
+//   }
+//   return arr[0]
+// }
+// /** 2 递归 改变了原链表
+//  * @param {ListNode} list1
+//  * @param {ListNode} list2
+//  * @return {ListNode}
+//  */
+// var mergeTwoLists = function (list1, list2) {
+//   if (!list1) {
+//     return list2
+//   } else if (!list2) {
+//     return list1
+//   } else if (list1.val < list2.val) {
+//     list1.next = mergeTwoLists(list1.next, list2)
+//     return list1
+//   } else {
+//     list2.next = mergeTwoLists(list1, list2.next)
+//     return list2
+//   }
+// }
+
+/** 3 迭代 改变了原链表
  * @param {ListNode} list1
  * @param {ListNode} list2
  * @return {ListNode}
  */
 var mergeTwoLists = function (list1, list2) {
-  const node = new ListNode()
-  while (list1.next || list2.next) {
-    if (list1.val < list2.val) {
+  const prev = new ListNode()
+  let node = prev
+  while (list1 || list2) {
+    if (!list1) {
+      node.next = list2
+      list2 = list2.next
+    } else if (!list2) {
+      node.next = list1
+      list1 = list1.next
+    } else if (list1.val < list2.val) {
+      node.next = list1
+      list1 = list1.next
+    } else {
+      node.next = list2
+      list2 = list2.next
     }
+    node = node.next
   }
+  // node.next = list1
+  return prev.next
 }
+
 // @lc code=end
